0180e4209a6505e392894f3a9a5adf996e23016b
Anthony Breder <awbreder@gmail.com>
2021-09-20 20:03:52 -0400

    updated json

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import bgImage from '../assets/doge.jpg'
+import bgImage from '../assets/bgimage.jpg'
 
 export default class Header extends React.Component {
   render() {
     return (
       <div className='header-bg'>
         <div><img id="background-image" src={bgImage} /></div>
-        <div className='header-content'>
+        {/* <div className='header-content'>
           <div id="title-text">2020-2021</div>
           <div id="subtitle-text">A Year In Review</div>
-        </div>
+        </div> */}
       </div>
     )
   }
